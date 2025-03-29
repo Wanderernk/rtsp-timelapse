e48389f5360d30614499c841c02ce3f7a42d8f06
Yan Kazachkov <yankazachkov@gmail.com>
2025-03-29 17:48:38 +0700

    Refactor code to support multiple IP cameras and update dependencies. Also added docker support.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ images_directory = "input"
 timelapse_directory = "output"
 
 
-def create_timelapse(force_framerate=False):
+def create_timelapse(subfolder, force_framerate=False):
     """
     Uses ffmpeg to stitch all images in a directory (in datetime order) together
     to make a timelapse.
@@ -28,7 +28,7 @@ def create_timelapse(force_framerate=False):
         timelapse_filename = (
             f"{datetime.now().strftime('%Y%m%d-%H%M%S')}_fps_{framerate}.mp4"
         )
-        timelapse_filepath = f"{timelapse_directory}/forced_fps/{timelapse_filename}"
+        timelapse_filepath = f"{timelapse_directory}/{subfolder}/forced_fps/{timelapse_filename}"
         subprocess.run(
             [
                 "ffmpeg",
@@ -37,76 +37,84 @@ def create_timelapse(force_framerate=False):
                 "-pattern_type",
                 "glob",
                 "-i",
-                f"{images_directory}/*.png",
-                f"{timelapse_directory}/forced_fps/{timelapse_filename}",
+                f"{images_directory}/{subfolder}/*.png",
+                f"{timelapse_directory}/{subfolder}/forced_fps/{timelapse_filename}",
             ]
         )
     else:
         print(f"Creating a normal timelapse")
         timelapse_filename = f"{datetime.now().strftime('%Y%m%d-%H%M%S')}.mp4"
-        timelapse_filepath = f"{timelapse_directory}/normal_fps/{timelapse_filename}"
+        timelapse_filepath = f"{timelapse_directory}/{subfolder}/normal_fps/{timelapse_filename}"
         subprocess.run(
             [
                 "ffmpeg",
                 "-pattern_type",
                 "glob",
                 "-i",
-                f"{images_directory}/*.png",
-                f"{timelapse_directory}/normal_fps/{timelapse_filename}",
+                f"{images_directory}/{subfolder}/*.png",
+                f"{timelapse_directory}/{subfolder}/normal_fps/{timelapse_filename}",
             ]
         )
     return timelapse_filepath
 
 
 def main():
+
+    # read array of credentials and ips from config file
     rtsp_username = config.rtsp_username
     rtsp_password = config.rtsp_password
-    rtsp_ip_address = config.rtsp_ip_address
-    rtsp_path = f"rtsp://{rtsp_username}:{rtsp_password}@{rtsp_ip_address}/stream1"
+    
+    rtsp_ip_address = config.ip_cameras
 
-    # Use ffmpeg to connect to the rtsp stream and save 1 frame
-    # ffmpeg -i <stream> -vframes 1 <output>
-    subprocess.run(
-        [
-            "ffmpeg",
-            "-i",
-            rtsp_path,
-            "-vframes",
-            "1",
-            f"{images_directory}/{datetime.now().strftime('%Y%m%d-%H%M%S')}.png",
-        ]
-    )
+    for camera_ip_address in rtsp_ip_address:
+        rtsp_path = f"rtsp://{rtsp_username}:{rtsp_password}@{camera_ip_address}/stream1"
+        camera_dir = f"{images_directory}/{camera_ip_address}"
+        if not os.path.exists(camera_dir):
+            os.makedirs(camera_dir)
 
-    # Only create a timelapse once we have a weeks worth of photos
-    image_files = glob.glob1(images_directory, "*.png")
-    expected_number_of_photos = 168
-    if len(image_files) >= expected_number_of_photos:
-        # Create the timelapse
-        normal_timelapse_filepath = create_timelapse()
-        forced_fps_timelapse_filepath = create_timelapse(force_framerate=True)
+        # Use ffmpeg to connect to the rtsp stream and save 1 frame
+        # ffmpeg -i <stream> -vframes 1 <output>
+        subprocess.run(
+            [
+                "ffmpeg",
+                "-i",
+                rtsp_path,
+                "-frames:v",
+                "1",
+                f"{camera_dir}/{datetime.now().strftime('%Y%m%d-%H%M%S')}.png",
+            ]
+        )
 
-        # Create an Apprise instance
-        app = apprise.Apprise()
+        # Only create a timelapse once we have a weeks worth of photos
+        image_files = glob.glob1(camera_dir, "*.png")
+        expected_number_of_photos = 168
+        if len(image_files) >= expected_number_of_photos:
+            # Create the timelapse
+            normal_timelapse_filepath = create_timelapse()
+            forced_fps_timelapse_filepath = create_timelapse(force_framerate=True)
 
-        for service in config.apprise_services:
-            app.add(service)
+            # Create an Apprise instance
+            app = apprise.Apprise()
 
-        attachments = [
-            normal_timelapse_filepath,
-            forced_fps_timelapse_filepath,
-        ]
+            for service in config.apprise_services:
+                app.add(service)
 
-        # Send the message to the Apprise services
-        print(f"Sending notification with {attachments}")
-        app.notify(body="Weekly timelapse", title="Timelapse", attach=attachments)
+            attachments = [
+                normal_timelapse_filepath,
+                forced_fps_timelapse_filepath,
+            ]
 
-        # Delete the images
-        print("Starting deletion")
-        for image_file in image_files:
-            image_filepath = f"{images_directory}/{image_file}"
-            if os.path.exists(image_filepath):
-                print(f"Deleting {image_filepath}")
-                os.remove(image_filepath)
+            # Send the message to the Apprise services
+            print(f"Sending notification with {attachments}")
+            app.notify(body="Weekly timelapse", title="Timelapse", attach=attachments)
+
+            # Delete the images
+            # print("Starting deletion")
+            # for image_file in image_files:
+            #     image_filepath = f"{images_directory}/{image_file}"
+            #     if os.path.exists(image_filepath):
+            #         print(f"Deleting {image_filepath}")
+            #         os.remove(image_filepath)
 
 
 if __name__ == "__main__":
