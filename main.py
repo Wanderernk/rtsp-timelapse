2b2781ccad3f35d8464fd27f1eac6ec5fa140440
Yan Kazachkov <yankazachkov@gmail.com>
2025-04-07 13:52:53 +0700

    Update ffmpeg concat filter with safe mode and modify index file path

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def create_timelapse_for_stream(subfolder, week_number, force_framerate=False):
             image_files += glob.glob(f"{images_directory}/{subfolder}/{week_number}/*-{day_number}-{hour:02d}*.png")
     image_files = sorted(image_files)
     
-    index_filename = f"{timelapse_directory}/{subfolder}/{time_moment}-index.txt"
+    index_filename = f"{time_moment}-index.txt"
     with open(index_filename, "w") as f:
         for image_file in image_files:
             f.write(f"file '{image_file}'\n")    
@@ -58,6 +58,8 @@ def create_timelapse_for_stream(subfolder, week_number, force_framerate=False):
                 # "glob",
                 "-f",
                 "concat",
+                "safe",
+                "0",
                 "-i",
                 # f"{images_directory}/{subfolder}/{week_number}/*.png",
                 f"'{index_filename}'",
@@ -112,6 +114,8 @@ def create_timelapse_for_stream(subfolder, week_number, force_framerate=False):
                 # "glob",
                 "-f",
                 "concat",
+                "safe",
+                "0",
                 "-i",
                 # f"{images_directory}/{subfolder}/{week_number}/*.png",
                 f"'{index_filename}'",
